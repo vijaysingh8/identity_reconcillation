91c7e799eaf3d94f5d676fa91e183dd3f980140f
Vijay <vijaybokan87@gmail.com>
2026-02-27 18:02:30 +0530

    setup express server and route middleware

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
