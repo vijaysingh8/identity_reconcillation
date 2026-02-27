b2fadf27566931e31ee11c1c7d09b857c6c779db
Vijay <vijaybokan87@gmail.com>
2026-02-27 18:40:52 +0530

    Setup Express server and route middleware

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 require("dotenv").config();
-
+const contactRoutes = require("./routes/contactRoutes");
 const app = express();
 app.use(express.json());
-
+app.use("/identify", contactRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
